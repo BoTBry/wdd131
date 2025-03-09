b26f1371dd4afcc3d931526097cc505e691ad8ec
BoTBry <abchigbu@gmail.com>
2025-03-09 07:31:26 -0700

    update

---
+++ week01/scripts/prac.js
@@ -0,0 +1,23 @@
+// alert("Hi, Welcome to my website.");
+let name = prompt("What is your name?")
+const bol1 = true;
+confirm("Are you sure?"), alert("Hi, " + name);
+console.log("Hello!! \nWelcome " + name);
+console.log(`Hello!! ${name}. \nWelcome`);
+
+console.log(typeof bol1);
+console.error("ERROR ERROR ERROR")
+console.warn("WARNNING SD")
+
+window.open("https://www.google.com");
+// # Download and install fnm:
+// winget install Schniz.fnm
+
+// # Download and install Node.js:
+// fnm install 22
+
+// # Verify the Node.js version:
+// node -v # Should print "v22.14.0".
+
+// # Verify npm version:
+// npm -v # Should print "10.9.2".
