ed95ffd4a0d93e5871203e50b18e2be688ee7a45
BoTBry <abchigbu@gmail.com>
2025-03-10 09:04:28 -0700

    mod

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,8 @@ const today = new Date();
 year.innerHTML = `${today.getFullYear()}`;
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Updated ${lastModified}` 
+
+console.warn("WARNNING SD");
+
+let name = prompt("What is your name?");
+confirm("Are you sure?"), alert(`Hello ${name}, Welcome to my page!!!`);
