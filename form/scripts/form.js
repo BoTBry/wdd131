c038c05930283b9a42bf457d5833348bcf22aeb6
BoTBry <abchigbu@gmail.com>
2025-04-15 22:16:09 +0100

    form

--- form/scripts/form.js
+++ form/scripts/form.js
@@ -13,13 +13,39 @@ const lastModified = document.lastModified;
 lastMod.innerHTML = `Last Updated ${lastModified}`;
 
 const opt_list = document.getElementById("slt_list");
-const item_list =["Flux Capacitor", "Power Laces", "Time Circuits", "Low Voltage Reactor", "Warp Equalizer"];
+const item_list = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
 
 let create_opt_list = (item) =>{
     item.forEach((element, index) => {
         const opts = document.createElement("option");
-        opts.textContent = element;
-        opts.setAttribute("value", element.toLowerCase());
+        opts.textContent = element.name;
+        opts.setAttribute("value", element.name.toLowerCase());
         opt_list.appendChild(opts);
         console.log(index);
     });
