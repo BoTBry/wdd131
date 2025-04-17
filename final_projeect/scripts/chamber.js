eca33af865ddc70e2e0c1004ee3083b91e8b1239
BoTBry <abchigbu@gmail.com>
2025-04-17 23:52:05 +0100

    done

--- final_projeect/scripts/chamber.js
+++ final_projeect/scripts/chamber.js
@@ -7,3 +7,17 @@ year.textContent = `${current_year.getFullYear()}`;
 menu_btn.addEventListener("click", ()=>{
     navigation.classList.toggle("show");
 })
+
+const tracker = () =>{
+    const Welcome = document.getElementById("Welcome");
+    let fs_visit = localStorage.setItem("fs-visit", Date.now());
+    let visit_tracker = Number(localStorage.getItem("visit-ct"));
+    visit_tracker++;
+    if (visit_tracker > 1){
+        Welcome.textContent = `Back So Soon!!😊`;
+    }
+    localStorage.setItem("visit-ct", visit_tracker);
+    let view_date = Number(localStorage.getItem("fs-visit"));
+    console.log(view_date);
+};
+tracker();
